cb8580381c60824f35c225bad7a68eb268812550
Rich0512 <jack3329tw@gmail.com>
2025-04-30 10:58:50 +0800

    fixed main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,9 @@
-import os
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.chat_router import router as chat_router
 from app.routers.agent_router import router as agent_router
 from app.routers.search_router import router as search_router
-
-
-DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 app = FastAPI()
 
